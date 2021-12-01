4565dad80bafece0739dd653e52c75e2cc9e3056
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-12-01 23:43:22 +0530

    ADDED : HTTP Method that is GET Request Method to begin with show Hello from bridgelabz.

--- src/main/java/com/bridgelabz/myspringapp/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/myspringapp/controller/HelloRestController.java
@@ -1,17 +1,23 @@
 package com.bridgelabz.myspringapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
- * @Purpose: To Create A Hello REST Controller to print messages.
- *
  * @author : VAISHNAVI R. VISHWAKARMA.
- * @since  : 1-12-2021.
+ * @Purpose: To Create A Hello REST Controller to print messages.
+ * @since : 1-12-2021.
  */
 @RestController
 public class HelloRestController {
     @RequestMapping("/")
     public String index() {
         return "Hello from BridgeLabz...!!";
+    }
+
+    @GetMapping(value = "/hello1")
+    public String getMethod() {
+        return "Hello from bridgelabz";
     }
 }
