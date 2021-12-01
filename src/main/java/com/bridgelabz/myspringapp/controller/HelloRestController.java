4cf427745b725742a37388c63403d03bfc8fec34
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-12-01 23:17:34 +0530

    ADDED : Java documentations to HelloRestController Class.

--- src/main/java/com/bridgelabz/myspringapp/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/myspringapp/controller/HelloRestController.java
@@ -2,7 +2,12 @@ package com.bridgelabz.myspringapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @Purpose: To Create A Hello REST Controller to print messages.
+ *
+ * @author : VAISHNAVI R. VISHWAKARMA.
+ * @since  : 1-12-2021.
+ */
 @RestController
 public class HelloRestController {
     @RequestMapping("/")
