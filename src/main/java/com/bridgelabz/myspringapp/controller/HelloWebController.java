e6b6191f531d8a3244c843c8e81d0866d2f91fe0
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-12-01 21:29:39 +0530

    ADDED : Create a Helloworld Spring App.

--- src/main/java/com/bridgelabz/myspringapp/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/myspringapp/controller/HelloWebController.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.myspringapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWebController {
-   @GetMapping("/web")
+    @GetMapping("/web")
     public String hello() {
-        return"hello";
+        return "hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model) {
+        model.addAttribute("message","This is a custom message");
+        return "message";
     }
 }
